68e52ce4cfb1eef85f19a219bcf5ad4a8f7763d1
chenghaoli36 <57383595+chenghaoli36@users.noreply.github.com>
2020-07-28 20:25:18 -0700

    fixed intake code so L side is intake, lmme know if thers any issues

--- src/subsystemFiles/intake.cpp
+++ src/subsystemFiles/intake.cpp
@@ -2,18 +2,12 @@
 void intakecontroller(){
   if(controller.get_digital(DIGITAL_L1)){
     leftintake.move_velocity(200);
+    rightintake.move_velocity(200);
   }else if(controller.get_digital(DIGITAL_L2)){
     leftintake.move_velocity(-200);
-  }else{
-    leftintake.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-    leftintake.move_velocity(0);
-  }
-  if(controller.get_digital(DIGITAL_R1)){
-    rightintake.move_velocity(200);
-  }else if(controller.get_digital(DIGITAL_R2)){
     rightintake.move_velocity(-200);
   }else{
+    leftintake.move_velocity(0);
     rightintake.move_velocity(0);
-    rightintake.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
   }
 }
