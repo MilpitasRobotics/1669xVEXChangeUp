0a403068e97fd024112251c1ccb5cfe6e3a78911
kathansheth04 <50056395+kathansheth04@users.noreply.github.com>
2020-10-11 12:23:33 -0700

    intakes with shift key experiment

--- src/subsystemFiles/intake.cpp
+++ src/subsystemFiles/intake.cpp
@@ -1,19 +1,21 @@
 #include "main.h"
+
+/**
+ * L1 is pressed for intaking. L1 and X is pressed for outtaking
+ * X is the shift key
+*/
+
 void intakecontroller(){
   if(controller.get_digital(DIGITAL_L1)){
-    leftintake.move_velocity(200);
-  }else if(controller.get_digital(DIGITAL_L2)){
-    leftintake.move_velocity(-200);
-  }else{
-    leftintake.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-    leftintake.move_velocity(0);
-  }
-  if(controller.get_digital(DIGITAL_R1)){
     rightintake.move_velocity(200);
-  }else if(controller.get_digital(DIGITAL_R2)){
+    leftintake.move_velocity(200);
+  }else if(controller.get_digital(DIGITAL_L1) && controller.get_digital(DIGITAL_X)){
+    leftintake.move_velocity(-200);
     rightintake.move_velocity(-200);
   }else{
-    rightintake.move_velocity(0);
+    leftintake.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
     rightintake.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+    leftintake.move_velocity(0);
+    rightintake.move_velocity(0);
   }
 }
